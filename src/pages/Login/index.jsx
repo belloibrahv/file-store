6958ca8106db0bace947e9b8c3da365aa14be3b8
belloibrahv <belloibrahv@gmail.com>
2022-05-17 12:17:04 -0700

    Modified the login page to meet specications
    - Added icons via google fonts
    - Added an illustrative image at the left side of the box
    - Added an image logo to the login page
    - Ensures the login page conforms with the required specification

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './Login.css'
-import { faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons' 
+import '../Login/index.css'
+import loginBg from '../Login/login.png'; 
+import facebook from '../Login/facebook.png'; 
+import google from '../Login/google.png';
+import twitter from '../Login/twitter.png';
+
 
 const Login = () => {
   
@@ -8,11 +12,16 @@ const Login = () => {
     <div className='login-box-wrapper'>
 
       <div className='login-box-inner'>
-        <div className='top-header-logo'>filestore</div>
+        <div className='top-header-logo'>
+          <div>
+            <img className='fs-logo' src="https://ghostlamp.com/wp-content/uploads/fav.png" alt="fs image"/>
+          </div>
+          <div>filestore</div>
+          </div>
         
         <div className='flexed-inner-box'>
           <div className='left-img-box'>
-            <img src="https://a.slack-edge.com/d9cde/marketing/img/features/inline/img-work-easily.png" alt="" />
+            <img src={loginBg} alt="Login Illustrative Background"/>
           </div>
           <div className='right-login-box'>
             <header className='welcome'>Welcome Back:)</header>
@@ -25,14 +34,14 @@ const Login = () => {
                 console.log('Submitted')
               }}>
                 <div className="email-box">
-                  <FontAwesomeIcon className='font-awesome' icon={faEnvelope} />
+                <span class="material-symbols-outlined lock">mail</span>
                   <div className='right-side-input'>
                     <label htmlFor="email">Email Address</label>
                     <input type="email" name='email' required/>
                   </div>
                 </div>
                 <div className="password-box">
-                  <FontAwesomeIcon className='font-awesome' icon={faLock} />
+                  <span class="material-symbols-outlined lock">lock</span>
                   <div className='right-side-input'>
                     <label htmlFor="password">Password</label>
                     <input type="password" name='password' required/>
@@ -44,14 +53,23 @@ const Login = () => {
                     <input type="checkbox" name="remenber-me"/> 
                     <div>Remember Me</div>
                   </div>
-                    <a className='forgot-pwd'>Forget Password?</a>
+                    <a href='https://www.google.com' className='forgot-pwd'>Forget Password?</a>
                 </div>
 
                 <div className='login-cra-btn'>
                   <button type='button' className='btn login-btn'>login now</button>
-                  <a className='btn cra-btn'>create account</a>
+                  <button type='button' className='btn cra-btn'>create account</button>
                 </div>
               </form>
+              
+              <div className="social-login">
+                <p>Or you can join with</p>
+                <div className='social'>
+                  <img src={google} alt="Google Icon" />
+                  <img src={facebook} alt="Facebook Icon" />
+                  <img src={twitter} alt="Twitter Icon" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
